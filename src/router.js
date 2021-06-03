fcfa41652ad40483a975f2f77c5323f93edaffbf
HieuNX <xuanbachrtc@gmail.com>
2021-06-03 18:27:26 +0700

    first

---
+++ src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import SignIn from './views/SignIn.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+        path: '/sign_in',
+        name: 'signin',
+        component: SignIn
+      },
+  ]
+})
